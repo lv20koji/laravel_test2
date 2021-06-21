13a4d680bdb09dda9d6cbee5a665b22d153671c3
lv20koji <lv20koji@gmail.com>
2021-06-21 09:51:37 +0900

    todolist
    my first commit

---
+++ database/migrations/2021_06_17_105202_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->date('due_date'); 
+
+            //未着手(1)をデフォルト
+            $table->integer('status')->default(1);
+
+            $table->timestamps(); //NULL値可能なcreated_atとupdated_atカラム追加
+
+            // 外部キーを設定する
+            $table->foreign('folder_id')->references('id')->on('folders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//フォルダー参照機能
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+//フォルダー新規作成機能
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
+
+//タスク作成機能
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+//タスク編集機能
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
